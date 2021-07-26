95a01b8284534226df3b607930a56a88e3b2d2d0
Axsos-code <85572679+Axsos-code@users.noreply.github.com>
2021-07-27 00:40:00 +0200

    Create 2.sh

---
+++ 2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pacman -S xorg xorg-apps xorg-xinit xorg-twm xdg-desktop-portal-kde kdialog kfind plasma plasma-nm plasma-pa kdeplasma-addons print-manager plasma-firewall dolphin konsole sddm libxcb qt5-virtualkeyboard ffmpegthumbs ffmpegthumbnailer kscreen powerdevil kdegraphics-thumbnailers libgsf kdeconnect
+systemctl enable sddm
+
+
