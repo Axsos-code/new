2edd24e12d698ea444b68b1704eb5750032047fe
Axsos-code <85572679+Axsos-code@users.noreply.github.com>
2021-07-27 01:28:19 +0200

    Delete kde.sh

--- kde.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pacman -S xorg xorg-apps xorg-xinit xorg-twm xdg-desktop-portal-kde kdialog kfind plasma plasma-nm plasma-pa kdeplasma-addons print-manager plasma-firewall dolphin konsole sddm libxcb qt5-virtualkeyboard ffmpegthumbs ffmpegthumbnailer kscreen powerdevil kdegraphics-thumbnailers libgsf kdeconnect
